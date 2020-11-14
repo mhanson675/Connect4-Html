b0ffb728b4cac962ca1485607ced0fb2598bc116
Scotty Dev <mhanson675@gmail.com>
2020-11-14 12:06:37 -0600

    Alternating player turns working. Checking for a win to the  left, right, and down working

--- connect4.js
+++ connect4.js
@@ -1,19 +1,23 @@
 
-const board = [
-    [00, 01, 02, 03, 04, 05, 06],
-    [10, 11, 12, 13, 14, 15, 16],
-    [20, 21, 22, 23, 24, 25, 26],
-    [30, 31, 32, 33, 34, 35, 36],
-    [40, 41, 42, 43, 44, 45, 46],
-    [50, 51, 52, 53, 54, 55, 56]
+let board = [
+    ["X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X"]
 ]
+
+let currentPlayer = "red";
 
 function selectColumn(column) {
 
     let playedCell = validColumn(column);
 
     if (playedCell !== null) {
-        claimCell(playedCell, 'red');
+        claimCell(playedCell, currentPlayer);
+        checkWin(playedCell);
+        changePlayer();
     } else {
         console.log("Can't play this column");
     }
@@ -34,5 +38,108 @@ function validColumn(column) {
 }
 
 function claimCell(cell, player) {
+    let index = cell.id.replace("cell", "");
+    let rowIndex = index.slice(0, 1);
+    let colIndex = index.slice(1, 2);
+    board[rowIndex][colIndex] = player;
     cell.classList.add(player + "-owned");
+}
+
+function changePlayer() {
+    if (currentPlayer === "red") {
+        currentPlayer = "yellow";
+    } else {
+        currentPlayer = "red";
+    }
+}
+
+function checkWin(playedCell) {
+
+    let cell = playedCell.id.replace("cell", "");
+    let rowIndex = parseInt(cell.slice(0, 1));
+    let colIndex = parseInt(cell.slice(1, 2));
+
+    switch (rowIndex) {
+        case 0:
+        case 1:
+        case 2:
+            checkWinDown(colIndex, rowIndex);
+            break;
+        case 3:
+        case 4:
+        case 5:
+            //check up
+            break;
+    }
+
+    switch (colIndex) {
+        case 0:
+        case 1:
+        case 2:
+            checkWinRight(colIndex, rowIndex);
+            break;
+        case 3:
+            checkWinLeft(colIndex, rowIndex);
+            checkWinRight(colIndex, rowIndex);
+            break;
+        case 4:
+        case 5:
+        case 6:
+            checkWinLeft(colIndex, rowIndex);
+            break;
+    }
+}
+
+function checkWinRight(colIndex, rowIndex) {
+    console.log("Checking right");
+
+    let match = true;
+    let checkColumn = colIndex;
+    //check to the right
+    while (match && (checkColumn - colIndex) < 4) {
+        if (board[rowIndex][checkColumn] !== currentPlayer) {
+            match = false;
+        }
+        checkColumn++;
+    }
+    
+    if (match) {
+        console.log("Winner to the right!");
+    }
+}
+
+function checkWinLeft(colIndex, rowIndex) {
+    console.log("Checking left");
+
+    let match = true;
+    let checkColumn = colIndex;
+
+    while (match && (colIndex -checkColumn) < 4 ) {
+        if (board[rowIndex][checkColumn] !== currentPlayer) {
+            match = false;
+        }
+        checkColumn--;
+    }
+
+    if (match) {
+        console.log("Winner to the Left!");
+    }
+}
+
+function checkWinDown(colIndex, rowIndex) {
+    console.log("Checking down");
+
+    let match = true;
+    let checkRow = rowIndex;
+    //check down
+    while (match && (checkRow - rowIndex) < 4) {
+        if (board[checkRow][colIndex] !== currentPlayer) {
+            match = false;
+        }
+        checkRow++;
+    }
+    
+    if (match) {
+        console.log("Winner down!");
+    }
 }
